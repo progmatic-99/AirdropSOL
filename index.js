f1d1ed14afdea601604cb8ba9cc8584be384191f
Shubham Upreti <shubham.upreti@gmail.com>
2022-01-08 19:25:26 +0530

    quest 2 completed

--- index.js
+++ index.js
@@ -12,3 +12,42 @@ const newPair = new Keypair();
 
 const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
 const secretKey = newPair._keypair.secretKey;
+
+const getWalletBalance = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const myWallet = await Keypair.fromSecretKey(secretKey);
+    const walletBalance = await connection.getBalance(
+      new PublicKey(myWallet.publicKey)
+    );
+    console.log(`=> For wallet address ${publicKey}`);
+    console.log(
+      `=> Wallet balance: ${parseInt(walletBalance) / LAMPORTS_PER_SOL}SOL`
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const airDropSol = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const walletKeyPair = await Keypair.fromSecretKey(secretKey);
+    const fromAirDropSignature = await connection.requestAirdrop(
+      new PublicKey(walletKeyPair.publicKey),
+      2 * LAMPORTS_PER_SOL
+    );
+
+    await connection.confirmTransaction(fromAirDropSignature);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const driverFunc = async () => {
+  await getWalletBalance();
+  await airDropSol();
+  await getWalletBalance();
+};
+
+driverFunc();
